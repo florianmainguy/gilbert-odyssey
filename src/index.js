cbeb306f8e178034811da16429fa68801a3d2547
Florian Mainguy <florian.mainguy@gmail.com>
2019-12-22 18:34:26 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import mapboxgl from 'mapbox-gl';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2hpZW5jaGllbjE2NjQiLCJhIjoiY2s0aDc0OHd2MTNpOTN0bzJyNjRzOWxpaiJ9.OAzfshgQGxK4VRYepd-ysw';
+
+class Application extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            lng: 5,
+            lat: 34,
+            zoom: 2
+        };
+    }
+
+    componentDidMount() {
+        const map = new mapboxgl.Map({
+          container: this.mapContainer,
+          style: 'mapbox://styles/mapbox/streets-v11',
+          center: [this.state.lng, this.state.lat],
+          zoom: this.state.zoom
+        });
+
+        map.on('move', () => {
+            this.setState({
+              lng: map.getCenter().lng.toFixed(4),
+              lat: map.getCenter().lat.toFixed(4),
+              zoom: map.getZoom().toFixed(2)
+            });
+        });
+    }
+
+    render() {
+        return (
+          <div>
+            <div className='sidebarStyle'>
+                <div>Longitude: {this.state.lng} | Latitude: {this.state.lat} | Zoom: {this.state.zoom}</div>
+            </div>
+            <div ref={el => this.mapContainer = el} className="mapContainer"/>
+          </div>
+        )
+    }
+}
+
+ReactDOM.render(<Application />, document.getElementById('app'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
