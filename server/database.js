5a1e0aaa4e7bde886b53cd1c5e6e28ed3fe20364
florianmainguy <florian.mainguy@gmail.com>
2020-01-22 18:15:13 +0100

    setup mongoDB atlas connexion

--- server/database.js
+++ server/database.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose')
 
-mongoose
+/*mongoose
     .connect('mongodb://127.0.0.1:27017/course-cycliste', { useNewUrlParser: true })
     .catch(e => {
         console.error('Connection error', e.message)
-    })
+    })*/
 
+const password = 'xWaxbKSJhFU5MD8T';
+
+mongoose
+    .connect(`mongodb+srv://Chienchien:${encodeURIComponent(password)}@cluster0-3g216.mongodb.net/test?retryWrites=true&w=majority`)
+    .catch(e => {
+        console.error('Connection error', e.message)
+    })
+    
 const db = mongoose.connection
 
 db.once('open', function () {
