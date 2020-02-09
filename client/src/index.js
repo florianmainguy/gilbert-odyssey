873c4a282871e0105923d1e54479274d60f35cce
florianmainguy <florian.mainguy@gmail.com>
2020-02-09 18:53:46 +0100

    Html/Css for Cyclist view

--- client/src/index.js
+++ client/src/index.js
@@ -138,6 +138,7 @@ class Map extends React.Component {
     }
 }
 
+/*
 class CyclistPic extends React.Component {
     render() {
         return (
@@ -166,6 +167,32 @@ class CyclistKeyWins extends React.Component {
             </div>
         )
     }
+}*/
+
+class CyclistKeyInfos extends React.Component {
+    render() {
+        return (
+            <div>
+                <div class="middle-container container">
+                    <div class="profile block">
+                        <a class="add-button" href="#28"><span class="icon entypo-plus scnd-font-color"></span></a>
+                        <div class="profile-picture big-profile-picture clear">
+                            <img width="150px" alt="Anne Hathaway picture" src="http://upload.wikimedia.org/wikipedia/commons/e/e1/Anne_Hathaway_Face.jpg" />
+                        </div>
+                        <h1 class="user-name">Anne Hathaway</h1>
+                        <div class="profile-description">
+                            <p class="scnd-font-color">Lorem ipsum dolor sit amet consectetuer adipiscing</p>
+                        </div>
+                        <ul class="profile-options horizontal-list">
+                            <li><a class="comments" href="#40"><p><span class="icon fontawesome-comment-alt scnd-font-color"></span>23</p></a></li>
+                            <li><a class="views" href="#41"><p><span class="icon fontawesome-eye-open scnd-font-color"></span>841</p></a></li>
+                            <li><a class="likes" href="#42"><p><span class="icon fontawesome-heart-empty scnd-font-color"></span>49</p></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 class CyclistHistory extends React.Component {
@@ -182,9 +209,7 @@ class Cyclist extends React.Component {
     render() {
         return (
             <div>
-                <CyclistPic />
-                <CyclistName />
-                <CyclistKeyWins />
+                <CyclistKeyInfos />
                 <CyclistHistory />
             </div>
         )
