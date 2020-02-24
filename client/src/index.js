98b09d0ae5b967bd565589f34305c3d7b7a0b2c1
florianmainguy <florian.mainguy@gmail.com>
2020-02-24 18:29:02 +0100

    Added link to races in Home view

--- client/src/index.js
+++ client/src/index.js
@@ -420,8 +420,8 @@ class HomeClassique extends React.Component {
         this.handleRace = this.handleRace.bind(this);
     }
 
-    handleRace() {
-        this.props.handlerRightUI('race','Milano-Sanremo');
+    handleRace(raceName) {
+        this.props.handlerRightUI('race', raceName);
     }
 
     render() {
@@ -429,11 +429,16 @@ class HomeClassique extends React.Component {
             <div className="home-classiques">
                 <h6>Pick a race</h6>
                 <ul className="classique-list">
-                    <a href="#" onClick={this.handleRace}><li className="flex-races"><h5 className="race-title">Milano-Sanremo</h5></li></a>
-                    <a href="#"><li className="flex-races"><h5 className="race-title">Ronde van Vlaanderen</h5></li></a>
-                    <a href="#"><li className="flex-races"><h5 className="race-title">Paris-Roubaix</h5></li></a>
-                    <a href="#"><li className="flex-races"><h5 className="race-title">Liège-Bastogne-Liège</h5></li></a>
-                    <a href="#"><li className="flex-races"><h5 className="race-title">Giro di Lombardia</h5></li></a>
+                    <a href="#" onClick={() => this.handleRace('Milano-Sanremo')}>
+                        <li className="flex-races"><h5 className="race-title">Milano-Sanremo</h5></li></a>
+                    <a href="#" onClick={() => this.handleRace('Ronde van Vlaanderen')}>
+                        <li className="flex-races"><h5 className="race-title">Ronde van Vlaanderen</h5></li></a>
+                    <a href="#" onClick={() => this.handleRace('Paris-Roubaix')}>
+                        <li className="flex-races"><h5 className="race-title">Paris-Roubaix</h5></li></a>
+                    <a href="#" onClick={() => this.handleRace('Liège-Bastogne-Liège')}>
+                        <li className="flex-races"><h5 className="race-title">Liège-Bastogne-Liège</h5></li></a>
+                    <a href="#" onClick={() => this.handleRace('Giro di Lombardia')}>
+                        <li className="flex-races"><h5 className="race-title">Giro di Lombardia</h5></li></a>
                 </ul>
             </div>
         )
