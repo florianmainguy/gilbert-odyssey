09c50b3e6a60951bdb0759d3c42f41528c7f2a55
florianmainguy <florian.mainguy@gmail.com>
2020-03-10 21:04:11 +0100

    Added build folder

---
+++ client/build/precache-manifest.14cfb3ac03c76695ab428e194ecbe728.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b04a5160cbf9f779de9597086aab32e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "004193d2441a4266be4f",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "7a1bb2cdc4fbd77e3c5d",
+    "url": "/static/css/main.515f4f62.chunk.css"
+  },
+  {
+    "revision": "004193d2441a4266be4f",
+    "url": "/static/js/2.e2da634c.chunk.js"
+  },
+  {
+    "revision": "f09321e517ab4567bb350ec8faae3b4d",
+    "url": "/static/js/2.e2da634c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7a1bb2cdc4fbd77e3c5d",
+    "url": "/static/js/main.f231ff35.chunk.js"
+  },
+  {
+    "revision": "570d22701e59b3bb8cbd",
+    "url": "/static/js/runtime-main.387d5f8e.js"
+  },
+  {
+    "revision": "cd6d11ef068a3f0d483a61b73044e4ea",
+    "url": "/static/media/default.cd6d11ef.png"
+  }
+]);
