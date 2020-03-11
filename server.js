6f07d3907939cd749cb4a3b7c3d8e471e0aeb8df
florianmainguy <florian.mainguy@gmail.com>
2020-03-11 20:42:06 +0100

    Changed server port

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const classiqueRouter = require('./routes/classique-router')
 const cyclistRouter = require('./routes/cyclist-router')
 const proxy = require('http-proxy-middleware')
 const app = express()
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
@@ -14,18 +15,24 @@ app.use(bodyParser.json())
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, 'client/build')));
+
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+    res.send('Hello World!')
+})
 
 app.use('/api', classiqueRouter)
 app.use('/api', cyclistRouter)
 
-app.listen(process.env.PORT || 3000)
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
 module.exports = function(app) {
     // add other server routes to path array
-    app.use(proxy(['/api' ], { target: 'http://localhost:3000' }));
+    app.use(proxy(['/api' ], { target: 'http://localhost:5000' }));
 }
