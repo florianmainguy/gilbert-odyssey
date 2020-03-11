4b92123b93c6b53f8e1b1c674c54f863998bd863
Florian Mainguy <florian.mainguy@gmail.com>
2020-03-11 10:42:41 +0100

    Removed baseUrl for deployment

--- client/src/api.js
+++ client/src/api.js
@@ -1,13 +1,11 @@
 import axios from 'axios'
 
-const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
-})
+const api = axios.create()
 
-export const getClassique = name => api.get(`/classiques/${name}`)
-export const getAllClassiques = () => api.get(`/classiques`)
-export const getCyclist = name => api.get(`/cyclists/${name}`)
-export const getAllCyclists = () => api.get(`/cyclists`)
+export const getClassique = name => api.get(`/api/classiques/${name}`)
+export const getAllClassiques = () => api.get(`/api/classiques`)
+export const getCyclist = name => api.get(`/api/cyclists/${name}`)
+export const getAllCyclists = () => api.get(`/api/cyclists`)
 
 const apis = {
     getClassique,
