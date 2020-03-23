cbb0bbe1d3daabe118cbd53feb48d726a16e585f
florianmainguy <florian.mainguy@gmail.com>
2020-03-23 12:13:48 +0100

    Added Search bar from SemanticUI

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,17 +1,77 @@
 import React from 'react';
+import _ from 'lodash'
+import { Search, Label } from 'semantic-ui-react'
 
 class HomeCyclist extends React.Component {
+    constructor(props) {
+        super(props);
+        this.cyclistSearch = [];
+        this.state = {
+            isLoading: false,
+            results: [],
+            value: ''
+        };
+        this.initialState = this.state;
+    }
+
+    resultRenderer = ({ title }) => <Label content={title} />
+
+    handleResultSelect = (e, { result }) => this.setState({ value: result.title })
+
+    handleSearchChange = (e, { value }) => {
+        this.setState({ isLoading: true, value })
+    
+        setTimeout(() => {
+            if (this.state.value.length < 1) return this.setState(this.initialState)
+        
+            const re = new RegExp(_.escapeRegExp(this.state.value), 'i')
+            const isMatch = (result) => re.test(result.title)
+        
+            this.setState({
+                isLoading: false,
+                results: _.filter(this.cyclistSearch, isMatch),
+            })
+        }, 300)
+    }
+
+    componentDidMount() {
+        this.cyclistSearch = this.props.cyclists.map( value => ({ "title": value.cyclist }));
+        //console.log(this.cyclistSearch);
+    }
+
     render() {
         return (
             <div className="home-cyclist">
                 <h6>Find a Cyclist:</h6>
                 <form className="cyclist-search">
                     <input type="search" placeholder="Search Cyclist"/>
+                    <Search
+                        className= "semantic-search"
+                        loading={this.state.isLoading}
+                        onResultSelect={this.handleResultSelect}
+                        onSearchChange={_.debounce(this.handleSearchChange, 500, {
+                            leading: true,
+                        })}
+                        results={this.state.results}
+                        value={this.state.value}
+                        resultRenderer={this.resultRenderer}
+                        {...this.props}
+                    />
                 </form>
             </div>
         )
     }
 }
+/*
+<div fallbackElement="[object Object]" class="ui search">
+    <div class="ui icon input">
+        <input type="text" value="" tabindex="0" class="prompt" autocomplete="off" />
+        <i aria-hidden="true" class="search icon"></i>
+    </div>
+        <div class="results transition">
+        <div class="message empty"><div class="header">No results found.</div></div>
+    </div>
+</div>*/
 
 class HomeClassique extends React.Component {
     constructor(props) {
